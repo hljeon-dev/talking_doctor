8494ce403379b88e541818614a65ad0ab32bb044
jjaenie <wogus990519@gmail.com>
2024-08-26 15:34:54 +0900

    initial project

---
+++ TalkingDoctor/src/main/java/com/talking_doctor/TalkingDoctor/TalkingDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.talking_doctor.TalkingDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkingDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkingDoctorApplication.class, args);
+	}
+
+}
---
+++ TalkingDoctor/src/test/java/com/talking_doctor/TalkingDoctor/TalkingDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talking_doctor.TalkingDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkingDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
